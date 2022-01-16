2ad52d62877e349bb78c4a9207f14092b6cefa47
Andre Almeida <andreluas.k@gmail.com>
2022-01-16 16:56:31 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
@@ -5,5 +5,6 @@ import com.devsuperior.dsmovie.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
+    User findByEmail(String email);
 }
